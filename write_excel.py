52901c2cb8966aa2759a5743890003deb7956aa0
antonio <ant.sbe@protonmail.com>
2024-10-08 16:23:58 +0200

    add: attendees data

--- write_excel.py
+++ write_excel.py
@@ -5,7 +5,16 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import holidays
 
+def read_attendees(filename="attendees.xlsx"):
+    wb = opyxl.load_workbook(filename)
+    ws = wb.active
 
+    attendees = []
+    for row in ws.iter_rows(min_row=3, max_row=ws.max_row, min_col=3, max_col=3):
+        for cell in row:
+            attendees.append(cell.value)
+    return attendees
+    
 
 # to install: holidays, openpyxl
 
@@ -17,6 +26,9 @@ days_in_month = calendar.monthrange(next_month.year, next_month.month)[1]
 slot_ranges = [[7, 12], [14, 16]]
 slot_duration = 1
 
+# Weekly day off
+weekly_day_off = {"Friday", "Saturday", "Sunday"}
+
 time_slots = []
 for start_hour, end_hour in slot_ranges:
     current_time = start_hour
@@ -25,6 +37,7 @@ for start_hour, end_hour in slot_ranges:
         minutes = int((current_time - hours) * 60)
         time_slots.append(f"{hours:02d}:{minutes:02d}")
         current_time += slot_duration
+
 
 
 # Excel
@@ -59,7 +72,7 @@ for day in range(1, days_in_month + 1):
     start_row = 4
     date = f"{next_month.year}-{next_month.month:02d}-{day:02d}"
     for n_time_slot in range(len(time_slots) + 1):
-        if calendar.day_name[calendar.weekday(next_month.year, next_month.month, day)] in {"Friday", "Saturday", "Sunday"} or date in holidays.US(years=next_month.year): #weekday >= 5
+        if calendar.day_name[calendar.weekday(next_month.year, next_month.month, day)] in weekly_day_off or date in holidays.US(years=next_month.year): #weekday >= 5
             ws.cell(row=start_row+n_time_slot, column=day+1).fill = opyxl.styles.PatternFill(start_color=color_holidays, end_color=color_holidays, fill_type="solid")
         else:
             ws.cell(row=start_row+n_time_slot, column=day+1).fill = opyxl.styles.PatternFill(start_color=color_workday, end_color=color_workday, fill_type="solid")
@@ -79,15 +92,19 @@ ws.conditional_formatting.add(f"B5:{opyxl.utils.get_column_letter(days_in_month 
 ws.conditional_formatting.add(f"B5:{opyxl.utils.get_column_letter(days_in_month + 1)}{len(time_slots) + 4}", rule_gray)
 
 
-thin_side = opyxl.styles.Side(border_style="thin", color="000000")  # Linea sottile, colore nero
+thin_side = opyxl.styles.Side(border_style="thin", color="000000")
 border = opyxl.styles.Border(left=thin_side, right=thin_side, top=thin_side, bottom=thin_side)
 for row in ws[f"A3:{opyxl.utils.get_column_letter(days_in_month + 1)}{len(time_slots) + 4}"]:
     for cell in row:
         cell.border = border
 
+
+
 # Team dayoff
-ws_new = wb.create_sheet(title="Saluto")
-ws_new["A3"] = "ciao"
+attendees = read_attendees()
+print(attendees)
+ws_new = wb.create_sheet(title="Attendees contraints")
+ws_new["A3"] = "Name"
 
 
 
